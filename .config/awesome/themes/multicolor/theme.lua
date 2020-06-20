6b4fba48f80914614fb1838792729a189b9b5de2
sujan parajuli <mailme.suzan77@gmail.com>
2020-06-20 15:42:50 +0545

    modified

--- .config/awesome/themes/multicolor/theme.lua
+++ .config/awesome/themes/multicolor/theme.lua
@@ -16,7 +16,7 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
-theme.wallpaper                                 = theme.confdir .. "/wall.jpg"
+theme.wallpaper                                 = theme.confdir .. "/wallpaper.jpg"
 theme.font                                      = "Terminus 8"
 theme.menu_bg_normal                            = "#000000"
 theme.menu_bg_focus                             = "#000000"
